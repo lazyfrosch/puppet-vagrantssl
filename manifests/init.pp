131b3bcb2ae48f8b7e0495488a9ad12af5c36244
Markus Frosch <markus@lazyfrosch.de>
2015-12-08 14:13:20 +0100

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+class vagrantpuppetssl {
+
+  $_options = [
+    '--cadir=/vagrant/.ca',
+    '--user=vagrant',
+  ]
+  $__options = join($_options, ' ')
+
+  # build local CA
+  # local certifcate will be put in /var/lib/puppet/ssl automatically
+  exec { 'puppet vagrant CA':
+    command => "puppet cert ${__options} generate '${::fqdn}'",
+    user    => 'root',
+    path    => $::path,
+    creates => "/var/lib/puppet/ssl/certs/${::fqdn}.pem"
+  }
+
+  # copy CRL locally
+  # what the Puppet agent does usually
+  file { 'puppet cached crl':
+    ensure => file,
+    path   => '/var/lib/puppet/ssl/crl.pem',
+    source => '/vagrant/.ca/ca_crl.pem',
+    owner  => 'puppet',
+    group  => 'puppet',
+  }
+
+}
